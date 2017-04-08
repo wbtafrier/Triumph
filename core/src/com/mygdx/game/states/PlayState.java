9cc5cf7cf3a80fe99ea4f9a4767eae5886262a65
TigerRampage <wbtafrier@gmail.com>
2017-04-08 18:57:24 -0400

    submit

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -1,19 +1,44 @@
 package com.mygdx.game.states;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.avatar.Avatar;
+import com.mygdx.game.util.Direction;
 
 public class PlayState extends State {
 	//private Bird bird;
+	private Avatar avatar;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
+        avatar = new Avatar();
         //bird = new Bird(50,100);
         //cam.setToOrtho();
     }
 
     @Override
     protected void handleInput() {
-
+    	if (Gdx.input.isKeyPressed(Keys.DPAD_LEFT) || Gdx.input.isKeyPressed(Keys.A)) {
+			avatar.setSprite(rm.left);
+			avatar.setDirection(Direction.LEFT);
+			avaX -= Gdx.graphics.getDeltaTime() * avaSpeed;
+		}
+		if (Gdx.input.isKeyPressed(Keys.DPAD_RIGHT) || Gdx.input.isKeyPressed(Keys.D)) {
+			avatar.setSprite(rm.right);
+			avatar.setDirection(Direction.RIGHT);
+			avaX += Gdx.graphics.getDeltaTime() * avaSpeed;
+		}
+		if (Gdx.input.isKeyPressed(Keys.DPAD_UP) || Gdx.input.isKeyPressed(Keys.W)) {
+			avatar.setSprite(rm.back);
+			avatar.setDirection(Direction.UP);
+			avaY += Gdx.graphics.getDeltaTime() * avaSpeed;
+		}
+		if (Gdx.input.isKeyPressed(Keys.DPAD_DOWN) || Gdx.input.isKeyPressed(Keys.S)) {
+			avatar.setSprite(rm.front);
+			avatar.setDirection(Direction.DOWN);
+			avaY -= Gdx.graphics.getDeltaTime() * avaSpeed;
+		}
     }
 
     @Override
--- core/src/com/mygdx/game/states/State.java
+++ core/src/com/mygdx/game/states/State.java
@@ -14,8 +14,6 @@ public abstract class State {
         this.gsm = gsm;
         cam = new OrthographicCamera();
         mouse = new Vector3();
-
-
     }
 
     protected abstract void handleInput();
