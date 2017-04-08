b9d412dd324938765d96f379c3025b05fa37f51b
John <wescottj98@gmail.com>
2017-04-07 20:53:35 -0400

    revert

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,6 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);//hi
+		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
