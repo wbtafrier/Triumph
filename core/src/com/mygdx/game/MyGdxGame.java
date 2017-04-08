f0df2455107d061b04b92743eac369f58262261d
John <wescottj98@gmail.com>
2017-04-08 17:12:15 -0400

    esfs

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -7,43 +7,45 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.mygdx.game.avatar.Avatar;
-import com.mygdx.game.util.Direction;
 
 public class MyGdxGame extends ApplicationAdapter {
+	
+	public static final int WIDTH = 480;
+	public static final int HEIGHT = 800;
+	
 	SpriteBatch batch;
-	Avatar avatar;
-	Sprite back;
-	Sprite backWalk1;
-	Sprite backWalk2;
-	Sprite front;
-	Sprite frontWalk1;
-	Sprite frontWalk2;
-	Sprite left;
-	Sprite leftWalk1;
-	Sprite leftWalk2;
-	Sprite right;
-	Sprite rightWalk1;
-	Sprite rightWalk2;
+	Sprite avatar;
+	Texture back;
+	Texture backWalk1;
+	Texture backWalk2;
+	Texture front;
+	Texture frontWalk1;
+	Texture frontWalk2;
+	Texture left;
+	Texture leftWalk1;
+	Texture leftWalk2;
+	Texture right;
+	Texture rightWalk1;
+	Texture rightWalk2;
 	float avaX = 0, avaY = 0, avaSpeed = 50.0f;
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		back = new Sprite(new Texture("back.png"));
-		backWalk1 = new Sprite(new Texture("backWalk1.png"));
-		backWalk2 = new Sprite(new Texture("backWalk2.png"));
-		front = new Sprite(new Texture("front.png"));
-		frontWalk1 = new Sprite(new Texture("frontWalk1.png"));
-		frontWalk2 = new Sprite(new Texture("frontWalk2.png"));
-		left = new Sprite(new Texture("left.png"));
-		leftWalk1 = new Sprite(new Texture("leftWalk1.png"));
-		leftWalk2 = new Sprite(new Texture("leftWalk2.png"));
-		right = new Sprite(new Texture("right.png"));
-		rightWalk1 = new Sprite(new Texture("rightWalk1.png"));
-		rightWalk2 = new Sprite(new Texture("rightWalk2.png"));
+		back = new Texture("back.png");
+		backWalk1 = new Texture("backWalk1.png");
+		backWalk2 = new Texture("backWalk2.png");
+		front = new Texture("front.png");
+		frontWalk1 = new Texture("frontWalk1.png");
+		frontWalk2 = new Texture("frontWalk2.png");
+		left = new Texture("left.png");
+		leftWalk1 = new Texture("leftWalk1.png");
+		leftWalk2 = new Texture("leftWalk2.png");
+		right = new Texture("right.png");
+		rightWalk1 = new Texture("rightWalk1.png");
+		rightWalk2 = new Texture("rightWalk2.png");
 		
-		avatar = new Avatar(front);
+		avatar = new Sprite(front, front.getWidth(), front.getHeight());
 	}
 
 	@Override
@@ -51,46 +53,42 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0.5f, 0.5f, 1);
 		
 		if (Gdx.input.isKeyPressed(Keys.DPAD_LEFT) || Gdx.input.isKeyPressed(Keys.A)) {
-			avatar.setSprite(left);
-			avatar.setDirection(Direction.LEFT);
+			avatar.setTexture(left);
 			avaX -= Gdx.graphics.getDeltaTime() * avaSpeed;
 		}
 		if (Gdx.input.isKeyPressed(Keys.DPAD_RIGHT) || Gdx.input.isKeyPressed(Keys.D)) {
-			avatar.setSprite(right);
-			avatar.setDirection(Direction.RIGHT);
+			avatar.setTexture(right);
 			avaX += Gdx.graphics.getDeltaTime() * avaSpeed;
 		}
 		if (Gdx.input.isKeyPressed(Keys.DPAD_UP) || Gdx.input.isKeyPressed(Keys.W)) {
-			avatar.setSprite(back);
-			avatar.setDirection(Direction.UP);
+			avatar.setTexture(back);
 			avaY += Gdx.graphics.getDeltaTime() * avaSpeed;
 		}
 		if (Gdx.input.isKeyPressed(Keys.DPAD_DOWN) || Gdx.input.isKeyPressed(Keys.S)) {
-			avatar.setSprite(front);
-			avatar.setDirection(Direction.DOWN);
+			avatar.setTexture(front);
 			avaY -= Gdx.graphics.getDeltaTime() * avaSpeed;
 		}
 		
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(avatar.getSprite(), avaX, avaY);
+		batch.draw(avatar, avaX, avaY);
 		batch.end();
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
-		back.getTexture().dispose();
-		backWalk1.getTexture().dispose();
-		backWalk2.getTexture().dispose();
-		front.getTexture().dispose();
-		frontWalk1.getTexture().dispose();
-		frontWalk2.getTexture().dispose();
-		left.getTexture().dispose();
-		leftWalk1.getTexture().dispose();
-		leftWalk2.getTexture().dispose();
-		right.getTexture().dispose();
-		rightWalk1.getTexture().dispose();
-		rightWalk2.getTexture().dispose();
+		back.dispose();
+		backWalk1.dispose();
+		backWalk2.dispose();
+		front.dispose();
+		frontWalk1.dispose();
+		frontWalk2.dispose();
+		left.dispose();
+		leftWalk1.dispose();
+		leftWalk2.dispose();
+		right.dispose();
+		rightWalk1.dispose();
+		rightWalk2.dispose();
 	}
 }
--- core/src/states/MenuState.java
+++ core/src/states/MenuState.java
@@ -1,5 +1,33 @@
 package states;
 
-public class MenuState {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.MyGdxGame;
 
+public class MenuState extends State {
+	private Texture background;
+    private Texture playBtn;
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playbtn.png");
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background,0,0, MyGdxGame.WIDTH,MyGdxGame.HEIGHT);
+        sb.draw(playBtn,((MyGdxGame.WIDTH/2) - (playBtn.getWidth()/2)),(MyGdxGame.HEIGHT/2));
+        sb.end();
 }
